398662cd34d3db2e043ea65b9767f3727b64e06e
kelsonic <kgadams93@gmail.com>
2016-08-16 13:19:15 +0200

    Add webView helper

---
+++ App/Components/Helpers/WebView.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  WebView,
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F6EF',
+    flexDirection: 'column'
+  }
+});
+
+class WebPage extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <WebView source={{uri: this.props.url}} />
+      </View>
+    );
+  }
+}
+
+WebPage.propTypes = {
+  url: React.PropTypes.string.isRequired
+};
+
+module.exports = WebPage;
