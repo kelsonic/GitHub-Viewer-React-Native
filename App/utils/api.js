c25a76010f391f37e15c9a883d2fced2244da2c3
kelsonic <kgadams93@gmail.com>
2016-08-16 13:17:30 +0200

    Add api call to GitHub and firebase

--- App/utils/api.js
+++ App/utils/api.js
@@ -1,12 +1,30 @@
-var api = {
+const api = {
   getRepos(username) {
     username = username.toLowerCase().trim();
     const url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
   },
-  getBio() {
+  
+  getBio(username) {
     username = username.toLowerCase().trim();
     const url = `https://api.github.com/users/${username}`;
     return fetch(url).then((res) => res.json());
+  },
+
+  getNotes(username){
+    username = username.toLowerCase().trim();
+    let url = `https://github-viewer-26fd2.firebaseio.com/${username}.json`;
+    return fetch(url).then((res)=> res.json());
+  },
+
+  addNote(username, note){
+    username = username.toLowerCase().trim();
+    let url = `https://github-viewer-26fd2.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
-}
+};
+
+export default api;
